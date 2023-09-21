6f8b3617264713ba5458f7b0f8f028bc16fdf7a7
Eze Gatica <git@ezegatica.com>
2023-09-21 19:37:33 -0300

    Intento para que funcione

--- next.config.js
+++ next.config.js
@@ -13,6 +13,6 @@ const nextConfig = {
     domains: ['i.ezegatica.com']
   },
 };
-const isProduction = process.env.NODE_ENV === 'production';
-
-module.exports = isProduction ? withPWA(nextConfig) : nextConfig; // Solo exporta PWA al estar en producción.
+// const isProduction = process.env.NODE_ENV === 'production';
+module.exports = withPWA(nextConfig);
+// module.exports = isProduction ? withPWA(nextConfig) : nextConfig; // Solo exporta PWA al estar en producción.
