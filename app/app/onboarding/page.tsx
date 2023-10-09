29ba8ea7c6fe23880f061fd16445717e8a0b0b14
Eze Gatica <git@ezegatica.com>
2023-10-08 22:53:50 -0300

    Empezando con onboarding

---
+++ app/app/onboarding/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Content, Header, Title } from '@components/headers';
+
+export default function MisRespuestas(): JSX.Element {
+  return (
+    <>
+      <Header>
+        <Title>Onboarding</Title>
+      </Header>
+      <Content>
+        <p>hola</p>
+      </Content>
+    </>
+  );
+}
