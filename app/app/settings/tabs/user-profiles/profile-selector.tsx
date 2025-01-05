44038f3adcac558a5da0a5a4e2c4ecf7e6bcf712
Eze Gatica <git@ezegatica.com>
2025-01-04 22:07:35 -0300

    Upgrade: headlessui

--- app/app/settings/tabs/user-profiles/profile-selector.tsx
+++ app/app/settings/tabs/user-profiles/profile-selector.tsx
@@ -1,4 +1,10 @@
-import { Listbox, Transition } from '@headlessui/react';
+import {
+  Listbox,
+  ListboxButton,
+  ListboxOption,
+  ListboxOptions,
+  Transition
+} from '@headlessui/react';
 import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid';
 import Image from 'next/image';
 import { Fragment, type JSX, useState } from 'react';
@@ -12,7 +18,7 @@ export default function Selector({ options }: { options: any[] }): JSX.Element {
       {({ open }) => (
         <>
           <div className="relative mt-2">
-            <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
+            <ListboxButton className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
               <span className="flex items-center">
                 <Image
                   src={proxy(selected.avatar)}
@@ -30,7 +36,7 @@ export default function Selector({ options }: { options: any[] }): JSX.Element {
                   aria-hidden="true"
                 />
               </span>
-            </Listbox.Button>
+            </ListboxButton>
 
             <Transition
               show={open}
@@ -39,19 +45,19 @@ export default function Selector({ options }: { options: any[] }): JSX.Element {
               leaveFrom="opacity-100"
               leaveTo="opacity-0"
             >
-              <Listbox.Options className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              <ListboxOptions className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
                 {options.map(person => (
-                  <Listbox.Option
+                  <ListboxOption
                     key={person.id}
-                    className={({ active }) =>
+                    className={({ focus }) =>
                       classNames(
-                        active ? 'bg-indigo-600 text-white' : 'text-gray-900',
+                        focus ? 'bg-indigo-600 text-white' : 'text-gray-900',
                         'relative cursor-default select-none py-2 pl-3 pr-9'
                       )
                     }
                     value={person}
                   >
-                    {({ selected: isSelected, active }) => (
+                    {({ selected: isSelected, focus }) => (
                       <>
                         <div className="flex items-center">
                           <Image
@@ -72,7 +78,7 @@ export default function Selector({ options }: { options: any[] }): JSX.Element {
                         {isSelected ? (
                           <span
                             className={classNames(
-                              active ? 'text-white' : 'text-indigo-600',
+                              focus ? 'text-white' : 'text-indigo-600',
                               'absolute inset-y-0 right-0 flex items-center pr-4'
                             )}
                           >
@@ -81,9 +87,9 @@ export default function Selector({ options }: { options: any[] }): JSX.Element {
                         ) : null}
                       </>
                     )}
-                  </Listbox.Option>
+                  </ListboxOption>
                 ))}
-              </Listbox.Options>
+              </ListboxOptions>
             </Transition>
           </div>
         </>
