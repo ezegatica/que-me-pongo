78220329fe9812b403c8ddd74824dc8fac540ca6
Eze Gatica <git@ezegatica.com>
2025-01-12 14:18:08 -0300

    Disabling cache on health

--- app/api/health/db/route.ts
+++ app/api/health/db/route.ts
@@ -1,10 +1,27 @@
 import { prisma } from '../../../db';
 
+export const fetchCache = 'force-no-store';
+export const dynamic = 'force-dynamic';
+export const revalidate = false;
+
 export const GET = async (): Promise<Response> => {
   try {
     // execute 1 + 1 query to check if the database connection is healthy
-    const result = await prisma.$queryRaw`SELECT 1 + 1`;
-    return Response.json({ message: 'Database connection is healthy', result });
+    const [result, user, lastReport] = await Promise.all([
+      prisma.$queryRaw`SELECT 1 + 1`,
+      prisma.user.count(),
+      prisma.report.findFirst({
+        orderBy: {
+          date: 'desc'
+        }
+      })
+    ]);
+    return Response.json({
+      message: 'Database connection is healthy',
+      result,
+      userCount: user,
+      lastReport: lastReport?.date
+    });
   } catch (error) {
     return Response.json({
       message: JSON.stringify(error),
